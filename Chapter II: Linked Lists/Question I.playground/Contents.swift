b8d3f3aa22ea52db9c933aa2c7759777c14ae9d5
Emil Shirima <emil.shirima@hotmail.com>
2016-09-29 20:41:21 -0400

    Added a multiple element insertion method

--- Chapter II: Linked Lists/Question I.playground/Contents.swift
+++ Chapter II: Linked Lists/Question I.playground/Contents.swift
@@ -1,5 +1,183 @@
-//: Playground - noun: a place where people can play
+//Question: Write code to remove duplicates from an unsorted linked list.
 
 import UIKit
 
-var str = "Hello, playground"
+class ListNode<T: Equatable & Hashable>
+{
+    var data: T?
+    var next: ListNode?
+    
+    init()
+    {
+        data = nil
+        next = nil
+    }
+    
+    init(data: T)
+    {
+        self.data = data
+        self.next = nil
+    }
+    
+    static func ==<T: Equatable>(lhs: ListNode<T>, rhs: ListNode<T>)-> Bool
+    {
+        return lhs.data == rhs.data
+    }
+}
+
+extension ListNode: Hashable
+{
+    var hashValue: Int{
+        return ObjectIdentifier(self).hashValue
+    }
+}
+
+class LinkedList<T: Equatable & Hashable>
+{
+    public typealias Node = ListNode<T>
+    private var head: Node?, tail: Node?
+    
+    init()
+    {
+        head = nil
+        tail = nil
+    }
+    
+    init(linkedList: LinkedList)
+    {
+        if linkedList.isEmpty()
+        {
+            head = nil
+            tail = nil
+            return
+        }
+        
+        self.head = linkedList.head
+        
+        var headIterator: Node? = head, otherIterator: Node? = linkedList.head?.next
+        
+        while otherIterator?.next != nil
+        {
+            headIterator?.next = otherIterator
+            
+            otherIterator = otherIterator?.next
+        }
+        
+        self.tail = otherIterator
+    }
+    
+    private func insertFront(data: T) // O(1)
+    {
+        if isEmpty()
+        {
+            head = Node(data: data)
+            tail = head
+        }
+        else
+        {
+            let tempNode: Node? = Node(data: data)
+            
+            tempNode?.next = head
+            
+            head = tempNode
+        }
+    }
+    
+    private func insertBack(data: T) // O(1)
+    {
+        if isEmpty()
+        {
+            head = Node(data: data)
+            tail = head
+        }
+        else
+        {
+            tail?.next = Node(data: data)
+            tail = tail?.next
+        }
+    }
+    
+    func insert(back elements: T ...) // Best: O(1) Worst: O(N)
+    {
+        for element in elements
+        {
+            insertBack(data: element)
+        }
+    }
+    
+    func insert(front elements: T ...) // Best: O(1) Worst: O(N)
+    {
+        for element in elements
+        {
+            insertFront(data: element)
+        }
+    }
+    
+    func removeDuplicates()
+    {
+        if isEmpty()
+        {
+            print("No duplicates in empty LinkedList")
+            return
+        }
+        
+        var elementSet: Set<T> = Set<T>()
+        
+        var previousIterator: Node? = head
+        var currentIterator: Node? = head?.next
+        
+        while currentIterator != nil
+        {
+            if elementSet.contains((currentIterator?.data)!)
+            {
+                previousIterator?.next = currentIterator?.next
+                currentIterator?.next = nil
+                currentIterator = nil
+                currentIterator = previousIterator?.next
+            }
+            else
+            {
+                elementSet.insert((currentIterator?.data)!)
+                previousIterator = currentIterator
+                currentIterator = currentIterator?.next
+            }
+        }
+    }
+    
+    func printContents() // Worst: O(N) Best: O(1)
+    {
+        if isEmpty()
+        {
+            print("Cannot print an empty LinkedList")
+            return
+        }
+        
+        var tempIterator: Node? = head
+        
+        while tempIterator != nil
+        {
+            guard let currentData: T = tempIterator?.data else { return }
+            
+            print("\(currentData)")
+            
+            tempIterator = tempIterator?.next
+        }
+    }
+    
+    private func isEmpty()-> Bool // O(1)
+    {
+        return head == nil ? true : false
+    }
+}
+
+let temp: LinkedList<Int> = LinkedList()
+temp.insert(back: 2,4,56,7,4,3,2,2,98,43,7,56)
+
+print("Before")
+temp.printContents()
+
+temp.removeDuplicates()
+print("After")
+temp.printContents()
+
+
