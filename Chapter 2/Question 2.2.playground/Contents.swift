42779e1a5ea526c433e431e22b8edcb40de7e84e
Adams Ombonga <adamsombonga@gmail.com>
2016-10-01 22:36:47 -0400

    Completed Question 2.2

--- Chapter 2/Question 2.2.playground/Contents.swift
+++ Chapter 2/Question 2.2.playground/Contents.swift
@@ -1,3 +1,100 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
+/******************************************************************************************/
+///////////////////////// Start LinkedList Implementation //////////////////////////////////
+/******************************************************************************************/
+// Source: Raywenderlich -- https://github.com/raywenderlich/swift-algorithm-club/tree/master/Linked%20List
+public class LinkedListNode<T> {
+    var value: T
+    var next: LinkedListNode?
+    weak var previous: LinkedListNode?
+
+    public init(value: T) {
+        self.value = value
+    }
+}
+
+public class LinkedList<T> {
+    public typealias Node = LinkedListNode<T>
+
+    fileprivate var head: Node?
+
+    public var isEmpty: Bool {
+        return head == nil
+    }
+
+    public var first: Node? {
+        return head
+    }
+
+    public var last: Node? {
+        if var node = head {
+            while case let next? = node.next {
+                node = next
+            }
+            return node
+        } else {
+            return nil
+        }
+    }
+
+    public var count: Int {
+        if var node = head {
+            var c = 1
+            while case let next? = node.next {
+                node = next
+                c += 1
+            }
+            return c
+        } else {
+            return 0
+        }
+    }
+
+    public func append(value: T) {
+        let newNode = Node(value: value)
+        if let lastNode = last {
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        } else {
+            head = newNode
+        }
+    }
+}
+/******************************************************************************************/
+///////////////////////// End LinkedList Implementation ////////////////////////////////////
+/******************************************************************************************/
+
+func kThElement( list: LinkedList<String>, k: Int ) -> LinkedListNode<String> {
+    var current = list.head, final: LinkedListNode<String> = LinkedListNode(value: "nil") // LinkedListNode<String>
+    if ( (list.head == nil) || (k < 1) ) {
+        return final // return nil
+    } else {
+        for _ in 0..<k { // Loop through until reaching the k - 1 element
+            if (current != nil) {
+                current = current?.next
+            } else {
+                return final
+                // return nil -- unable to reach element at k - 1
+            }
+        }
+        // Return and assign the next element (kth element)
+        if current?.next != nil {
+            final = (current?.next)!
+            return final
+        }
+    }
+    return final // Should be un-reachable
+}
+
+// Test Case:
+let list = LinkedList<String>()
+list.append(value: "Hello")
+list.append(value: "World")
+list.append(value: "Test")
+
+
+print(kThElement(list: list, k: 2).value)
+
