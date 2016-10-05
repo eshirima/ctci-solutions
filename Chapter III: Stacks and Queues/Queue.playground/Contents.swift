03b49ee0bb0c3b003a1001b341d3544bdddda0d1
Emil Shirima <emil.shirima@hotmail.com>
2016-10-05 18:41:21 -0400

    Finalized implementation of stack datatype

--- Chapter III: Stacks and Queues/Queue.playground/Contents.swift
+++ Chapter III: Stacks and Queues/Queue.playground/Contents.swift
@@ -3,3 +3,37 @@
 import UIKit
 
 var str = "Hello, playground"
+
+struct GeneralUser
+{
+    var name: String = "", lastName: String?, number: String = "1234"
+    
+    init()
+    {
+        
+    }
+    
+    init(userName: String, lastName: String = "")
+    {
+        name = userName
+    }
+}
+
+class CurrentUser: NSObject
+{
+    var details: GeneralUser? = nil
+    
+    static let sharedInstance: CurrentUser = CurrentUser()
+    
+    fileprivate override init()
+    {
+        super.init()
+    }
+}
+
+let temp = CurrentUser.sharedInstance
+var userDetails = GeneralUser(userName: "Hello")
+userDetails.number = "442324333"
+temp.details = userDetails
+
+print(temp.details?.name)
--- Chapter III: Stacks and Queues/Stack.playground/Contents.swift
+++ Chapter III: Stacks and Queues/Stack.playground/Contents.swift
@@ -1,5 +1,134 @@
-//: Playground - noun: a place where people can play
+//: STACK IMPLEMENTATION
 
 import UIKit
 
-var str = "Hello, playground"
+class ListNode<T: Equatable>
+{
+    var data: T?
+    var next: ListNode?
+    
+    init()
+    {
+        data = nil
+        next = nil
+    }
+    
+    init(data: T)
+    {
+        self.data = data
+        self.next = nil
+    }
+    
+    static func ==<T: Comparable>(lhs: ListNode<T>, rhs: ListNode<T>)-> Bool
+    {
+        return lhs.data == rhs.data
+    }
+}
+
+class Stack<T: Equatable>
+{
+    public typealias Node = ListNode<T>
+    private var top: Node?
+    
+    init()
+    {
+        top = nil
+    }
+    
+    init(stack: Stack)
+    {
+        if stack.isEmpty()
+        {
+            top = nil
+            return
+        }
+        
+        self.top = stack.top
+        
+        var topIterator: Node? = top, otherIterator: Node? = stack.top?.next
+        
+        while otherIterator?.next != nil
+        {
+            topIterator?.next = otherIterator
+            
+            otherIterator = otherIterator?.next
+        }
+    }
+    
+    func peek()->T?
+    {
+        if isEmpty()
+        {
+            print("Can't peek an empty stack")
+            return nil
+        }
+        
+        guard let topData = top?.data else { return nil }
+        
+        return topData
+    }
+    
+    private func push(data: T) // O(1)
+    {
+        if isEmpty()
+        {
+            top = Node(data: data)
+        }
+        else
+        {
+            let tempNode: Node? = Node(data: data)
+            
+            tempNode?.next = top
+            
+            top = tempNode
+        }
+    }
+    
+    func push(elements: T...) // Worst: O(N) Best: O(1)
+    {
+        for element in elements
+        {
+            push(data: element)
+        }
+    }
+    
+    func pop() -> T?
+    {
+        if isEmpty()
+        {
+            print("Can't delete from an empty stack")
+            return nil
+        }
+        
+        let tempHolder: Node? = top
+        
+        top = top?.next
+        
+        return tempHolder?.data
+    }
+    
+    private func isEmpty()-> Bool // O(1)
+    {
+        return top == nil ? true : false
+    }
+    
+    func printContents() // Worst: O(N) Best: O(1)
+    {
+        if isEmpty()
+        {
+            print("Cannot print an empty LinkedList")
+            return
+        }
+        
+        var tempIterator: Node? = top
+        
+        while tempIterator != nil
+        {
+            guard let currentData: T = tempIterator?.data else { return }
+            
+            print("\(currentData)")
+            
+            tempIterator = tempIterator?.next
+        }
+    }
+}
